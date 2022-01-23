65e1bbda390a42aa3ec21244826cb2a853d176a0
arnaud <arnaud@ritual.co>
2022-01-22 22:59:38 -0500

    Generate Crypto Wallet use case

---
+++ domain/src/main/java/ca/arnaud/domain/model/Wallet.kt
@@ -0,0 +1,6 @@
+package ca.arnaud.domain.model
+
+data class Wallet(
+    val publicKey: String,
+    val privateKey: String
+)
---
+++ domain/src/main/java/ca/arnaud/domain/usecase/GenerateCryptoWallet.kt
@@ -0,0 +1,39 @@
+package ca.arnaud.domain.usecase
+
+import ca.arnaud.domain.executor.JobExecutorProvider
+import ca.arnaud.domain.model.Wallet
+import org.bitcoinj.crypto.ChildNumber
+import org.bitcoinj.crypto.DeterministicKey
+import org.bitcoinj.crypto.HDUtils
+import org.bitcoinj.wallet.DeterministicKeyChain
+import org.bitcoinj.wallet.DeterministicSeed
+import org.web3j.crypto.Credentials
+import org.web3j.utils.Numeric
+import javax.inject.Inject
+
+class GenerateCryptoWallet @Inject constructor(
+    jobExecutorProvider: JobExecutorProvider
+) : SuspendableUseCase<GenerateCryptoWallet.Params, Wallet>(jobExecutorProvider) {
+
+    data class Params(
+        val words: String,
+        val password: String = ""
+    )
+
+    override suspend fun buildRequest(params: Params): Wallet {
+        val seed = DeterministicSeed(params.words, null, params.password, 1409478661L)
+        val chain = DeterministicKeyChain.builder().seed(seed).build()
+        val keyPath: List<ChildNumber> = HDUtils.parsePath("M/44H/60H/0H/0/0")
+        val key: DeterministicKey = chain.getKeyByPath(keyPath, true)
+        val privateKey = Numeric.toHexStringNoPrefix(key.privKey)
+
+        // Web3
+        val credentials: Credentials = Credentials.create(privateKey)
+        val walletId = credentials.address
+
+        return Wallet(
+            publicKey = walletId,
+            privateKey = privateKey
+        )
+    }
+}
